750ba0a8fb5538845fc766b40ca74dd0e879e077
unknown <heiko.hardt@w410-1000771.bitlab.de>
2014-09-12 15:13:51 +0200

    Initial Commit

---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'FPDi',
+	'description' => 'Zusammenführung der FDF und FPDi Framework',
+	'category' => 'templates',
+	'author' => 'Heiko Hardt',
+	'author_email' => 'heiko.hardt@pixelpark.com',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 1,
+	'version' => '1.5.2',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.1'
+		),
+		'conflicts' => array(),
+		'suggests' => array(),
+	),
+);
